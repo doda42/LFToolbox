ac81f0144644058639463be296a1951f32833e52
Donald Dansereau <donald.dansereau@gmail.com>
2018-03-21 14:30:52 -0700

    Changed behaviour to better match matlab's when creating new figures for LFDisp*.  You can now have multiple open interactive/animated figures.

--- SupportFunctions/LFDispSetup.m
+++ SupportFunctions/LFDispSetup.m
@@ -26,6 +26,8 @@
 % 
 %     FigureHandle, ImageHandle : handles of the created objects
 %
+% todo: doc: changed behaviour to better match matlab's when creating new figures
+% you can now have multiple open interactive lfdisp figures
 %
 % See also:  LFDispVidCirc, LFDispMousePan
 
@@ -34,31 +36,51 @@
 
 function [ImageHandle, FigureHandle] = LFDispSetup( InitialFrame, ScaleFactor )
 
-FigureHandle = findobj('tag','LFDisplay');
-if( isempty(FigureHandle) )
+%---
+ScaleFactor = LFDefaultVal('ScaleFactor', 1);
 
+%---
+CurSize = [size(InitialFrame), ScaleFactor];
+FigureHandle = get(0, 'CurrentFigure');
+
+%---Check if window already existed with correct size---
+RedoWindow = false;
+if( ~isempty(FigureHandle) )
+	PrevSize = FigureHandle.UserData;
+	PrevTag = get(FigureHandle, 'Tag');
+	if( strcmp(PrevTag, 'LFDisplay') || ~all(size(PrevSize)==size(CurSize)) || ~all(CurSize == PrevSize) )
+		% either not tagged or incorrect size; clear the figure and rebuild it below
+		clf( FigureHandle );
+		RedoWindow = true;
+	end
+end
+
+if( isempty(FigureHandle) || RedoWindow )
     % Get screen size
     set( 0, 'units','pixels' );
     ScreenSize = get( 0, 'screensize' );
     ScreenSize = ScreenSize(3:4);
     
-    % Get LF display size
-    FrameSize = [size(InitialFrame,2),size(InitialFrame,1)];
-    
-    % Create the figure
-    FigureHandle = figure(...
-        'doublebuffer','on',...
-        'backingstore','off',...
-        ...%'menubar','none',...
-        ...%'toolbar','none',...
-        'tag','LFDisplay');
-    
-    % Set the window's position and size
-    WindowPos = get( FigureHandle, 'Position' );
-    WindowPos(3:4) = FrameSize;
-    WindowPos(1:2) = floor( (ScreenSize - FrameSize)./2 );
-    set( FigureHandle, 'Position', WindowPos );
-    
+    % Create the figure if one doesn't already exist
+	if( isempty(FigureHandle) )
+		FigureHandle = figure(...
+			'doublebuffer','on',...
+			'backingstore','off',...
+			...%'menubar','none',...
+			...%'toolbar','none',...
+			'tag','LFDisplay');
+	else
+		% make sure it's set up correctly
+		FigureHandle.set( 'doublebuffer','on',...
+			'backingstore','off',...
+			...%'menubar','none',...
+			...%'toolbar','none',...
+			'tag','LFDisplay');
+	end
+
+	FigureHandle.UserData = [size(InitialFrame), ScaleFactor];
+	FigureHandle.NextPlot = 'add';
+
     % Set the axis position and size within the figure
     AxesPos = [0,0,size(InitialFrame,2),size(InitialFrame,1)];
     axes('units','pixels',...
@@ -71,10 +93,11 @@ if( isempty(FigureHandle) )
          'layer','bottom');
      
     ImageHandle = imshow(InitialFrame);
-    % If a scaling factor is requested, apply it
-    if( exist('ScaleFactor','var') )
-        truesize(floor(ScaleFactor*size(InitialFrame(:,:,1))));
-    end
+	
+    % Scaling factor sets the size of the display
+	% this has the nice side-effect of rescaling and, if needed, repositining the window
+	truesize(floor(ScaleFactor*size(InitialFrame(:,:,1))));
+    
 else
     ImageHandle = findobj(FigureHandle,'type','image');
     set(ImageHandle,'cdata', InitialFrame);
