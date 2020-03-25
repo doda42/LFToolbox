a305526b686c2423b2cf6de9f53c35f56b22f705
Donald Dansereau <donald.dansereau@gmail.com>
2020-03-25 18:53:36 +1100

    updating documentation

--- LFDisp.m
+++ LFDisp.m
@@ -1,24 +1,29 @@
-% LFDisp - Convenience function to display a 2D slice of a light field
-% 
-% Usage: 
-%     LFSlice = LFDispMousePan( LF )
-%               LFDispMousePan( LF )
-%     
-% 
-% The centermost image is taken in s and t. Also works with 3D arrays of images. If an output argument is included, no
-% display is generated, but the extracted slice is returned instead.
-% 
-% Inputs:
-% 
-%     LF : a colour or single-channel light field, and can a floating point or integer format. For
-%          display, it is scaled as in imagesc.  If LF contains more than three colour
-%          channels, as is the case when a weight channel is present, only the first three are used.
-% 
-% 
-% Outputs:
-% 
-%     LFSlice : if an output argument is used, no display is generated, but the extracted slice is returned.
+% LFDisp - Convenience function to display a 2D slice of a light field using imagesc
 %
+% Usage:
+%      LFDisp( LF )
+%      [LFSlice, WOut] = LFDisp( LF, Renormalize )
+%
+% The centermost image is taken in s and t. Also works with 3D arrays of images. If an output
+% argument is included, no display is generated, but the extracted slice is returned instead.
+%
+% Inputs:
+%
+%      LF : a colour or single-channel light field, can be floating point or integer format. For
+%           display, it is scaled as in imagesc.  If LF contains more than three colour channels, as
+%           is the case when a weight channel is present, only the first three are used.
+%
+% Optional Inputs:
+%
+%      Renormalize : optionally rescale the image intensities between 0 and 1, only applies for
+%                    float inputs
+%
+% Outputs:
+%
+%     LFSlice : if an output argument is used, no display is generated, but the extracted slice is
+%               returned.
+%
+%     WOut : for inputs with a weight channel, the weight is stripped and returned separately
 %
 % See also:  LFDispVidCirc, LFDispMousePan
 
@@ -48,7 +53,7 @@ if( HasWeight )
 	if( nargout >= 2 )
 		WOut = LF(:,:,end);
 	end
-    LF = squeeze(LF(:,:,1:end-1));
+	LF = squeeze(LF(:,:,1:end-1));
 end
 
 if( Renormalize )
