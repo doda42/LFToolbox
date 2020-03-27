d54ecaa10074893b1a0d7d866f4c894299f2a62d
Donald Dansereau <donald.dansereau@gmail.com>
2020-03-27 17:55:08 +1100

    LFDispProj improved interface consistency, documentation; Dim1, Dim2 now specify which dimensions to project onto, not which to remove; arguments get passed through to LFDisp.

--- LFDispProj.m
+++ LFDispProj.m
@@ -1,32 +1,89 @@
-% Display a 4D light field as projection onto planes.
+% Project a 4D light field onto two planes, and optionally display it to the screen.
+%
+% Usage: 
+%     [Img,XL,YL] = LFDispProj( LF, [Dim1], [Dim2], [ProjectionMethod], [<LFDisp options>] )
+% 
+% All parameters except LF are optional. If an output argument is assigned, the result is not
+% displayed to the screen. 
+%
+% Inputs :
+% 
+%     LF : a light field to display.
+% 
+% Optional Inputs :
+% 
+%           Dim1, Dim2 : indices for each of the two dimensions to project onto; Defaults 3, 4.
+%
+%     ProjectionMethod : One of 'sum', 'mean', 'max', or 'min'; how the remaining two dimensions are
+%                        to be collapsed. Default: 'mean'.
+% 
+%    <LFDisp options> : any additional parameters get passed along to LFDisp
+%
+% Outputs : 
+%
+%     Img: A 2D bitmap of the generated image.
+% 
+%     XL, YL: labels for the x and y axes.
+% 
+% Examples :
+% 
+%     LFDispProj( LF );              % projects LF onto dimensions 3,4 and displays to screen
+% 
+%     LFDispProj( LF, 1, 3, 'max' ); % projects onto dimensions 1,2 by taking the max
+% 
+%     I = LFDispProj( LF );          % returns projection as I, does not display to screen
+%     LFDisp( I, 'Renormalize' );    % display I with renormalisation
+% 
+%     % project onto dims 1,2 with default projection method, enable renormalisation in LFDisp
+%     LFDispProj( LF, 1,2, [], 'Renormalize' );
+% 
+% See also: LFDispProjSubfigs, LFDispTiles, LFDispTilesSubfigs, LFDispVidCirc, LFDispMousePan
 
 % Part of LF Toolbox xxxVersionTagxxx
 % Copyright (C) 2012-2018 by Donald G. Dansereau
 
-function Img = LFXShowLF_proj( LF, Plane1, Plane2, Mode, varargin )
+function [Img,XL,YL] = LFDispProj( LF, Dim1, Dim2, ProjectionMethod, varargin )
 
-%---
-Mode = LFDefaultVal('Mode', 'sum');
+Dim1 = LFDefaultVal('Dim1', 3);
+Dim2 = LFDefaultVal('Dim2', 4);
+ProjectionMethod = LFDefaultVal('ProjectionMethod', 'mean');
 
-%---
-if( Plane1 > Plane2 )
-	t = Plane1;
-	Plane1 = Plane2;
-	Plane2 = t;
+
+%---Set labels, always display in same order---
+if( Dim1 > Dim2 )
+	t = Dim1;
+	Dim1 = Dim2;
+	Dim2 = t;
+end
+DimLabels = ['t','s','v','u'];
+XL = DimLabels(Dim2);
+YL = DimLabels(Dim1);
+
+%---Find the two dims to remove---
+RemoveDims = 1:4;
+RemoveDims([Dim1, Dim2]) = [];
+
+% post-Dim1 squeeze, Dim2 will be lesser by one
+RemoveDims(2) = RemoveDims(2) - 1;
+
+switch( lower(ProjectionMethod) )
+	case 'sum'
+		Img = squeeze(sum(squeeze(sum(LF,RemoveDims(1))), RemoveDims(2)));
+	case 'mean'
+		Img = squeeze(mean(squeeze(mean(LF,RemoveDims(1))), RemoveDims(2)));
+	case 'max'
+		Img = squeeze(max(squeeze(max(LF,[],RemoveDims(1))),[],RemoveDims(2)));
+	case 'min'
+		Img = squeeze(min(squeeze(max(LF,[],RemoveDims(1))),[],RemoveDims(2)));
+	otherwise
+		error('Unrecognized projection method');
 end
 
-Plane2 = Plane2 - 1;
-
-switch( lower(Mode) )
-    case 'sum'
-        Img = squeeze(sum(squeeze(sum(LF,Plane1)), Plane2));
-    case 'max'
-        Img = squeeze(max(squeeze(max(LF,[],Plane1)),[],Plane2));
-    case 'min'
-        Img = squeeze(min(squeeze(max(LF,[],Plane1)),[],Plane2));        
-end
-
-if( nargout == 0 )
-    LFDisp(Img,true);
+%---Display-----------------------------------------------------------------------------------------
+if( nargout < 1 )
+	LFDisp(Img, varargin{:});
 	clear Img
+	
+	xlabel(XL);
+	ylabel(YL);
 end
