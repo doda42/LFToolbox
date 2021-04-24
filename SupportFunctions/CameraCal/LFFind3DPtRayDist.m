38885e5666bd307805f4cb256f170f5e17ca6e58
Donald Dansereau <donald.dansereau@gmail.com>
2021-04-24 18:15:20 +1000

    find closest pt as well as dist in LFFind3DPtRayDist

--- SupportFunctions/CameraCal/LFFind3DPtRayDist.m
+++ SupportFunctions/CameraCal/LFFind3DPtRayDist.m
@@ -1,15 +1,15 @@
 % todo[doc]
 
 %---Compute distances from 3D rays to a 3D points---
-function [Dist] = LFFind3DPtRayDist( PtOnRay, RayDir, Pt3D )
+function [Dist, ClosestPt] = LFFind3DPtRayDist( PtOnRay, RayDir, Pt3D )
 
 RayDir = RayDir ./ repmat(sqrt(sum(RayDir.^2)), 3,1); % normalize ray
 Pt3D = Pt3D - PtOnRay;    % Vector to point
 
-PD1 = dot(Pt3D, RayDir);
-PD1 = repmat(PD1,3,1).*RayDir; % Project point vec onto ray vec
+ClosestPt = dot(Pt3D, RayDir);
+ClosestPt = repmat(ClosestPt,3,1).*RayDir; % Project point vec onto ray vec
 
-Pt3D = Pt3D - PD1;
+Pt3D = Pt3D - ClosestPt;
 Dist = sqrt(sum(Pt3D.^2, 1)); % Distance from point to projected point
 
 end
