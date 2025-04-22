0764589371a4b97e4ecb539891fea94453d17661
Donald Dansereau <donald.dansereau@gmail.com>
2025-04-22 14:51:18 +1000

    LFDefaultField added support for nested structs

--- SupportFunctions/LFDefaultField.m
+++ SupportFunctions/LFDefaultField.m
@@ -1,40 +1,42 @@
 % LFDefaultField - Convenience function to set up structs with default field values
-% 
-% Usage: 
-% 
+%
+% Usage:
+%
 %   ParentStruct = LFDefaultField( ParentStruct, FieldName, DefaultVal )
-% 
+%
 % This provides an elegant way to establish default field values in a struct. See LFDefaultValue for
-% setting up non-struct variables with default values. 
-% 
+% setting up non-struct variables with default values.
+%
 % Inputs:
-% 
+%
 %   ParentStruct: string giving the name of the struct; the struct need not already exist
 %   FieldName: string giving the name of the field
 %   DefaultVal: default value for the field
 %
 % Outputs:
-% 
+%
 %   ParentStruct: if the named struct and field already existed, the output matches the struct's
 %                 original value; otherwise the output is a struct with an additional field taking
 %                 on the specified default value
-% 
-% Example: 
-% 
+%
+% Example:
+%
 %   clearvars
 %   ExistingStruct.ExistingField = 42;
 %   ExistingStruct = LFDefaultField( 'ExistingStruct', 'ExistingField', 3 )
 %   ExistingStruct = LFDefaultField( 'ExistingStruct', 'NewField', 36 )
 %   OtherStruct = LFDefaultField( 'OtherStruct', 'Cheese', 'Indeed' )
-%   
-% 
+%   TopStruct.NestedStruct = LFDefaultField( 'TopStruct.NestedStruct', 'HasAField', 'yes' )
+%
 %   Results in :
-%       ExistingStruct = 
+%       ExistingStruct =
 %           ExistingField: 42
 %                NewField: 36
-%       OtherStruct = 
-%                  Cheese: 'Indeed'
-%
+%       OtherStruct =
+%                Cheese: 'Indeed'
+%       TopStruct.NestedStruct =
+%                HasAField: 'yes'
+% 
 % Usage for setting up default function arguments is demonstrated in most of the LF Toolbox
 % functions.
 %
@@ -42,16 +44,23 @@
 % See also: LFDefaultVal
 
 % Copyright (c) 2013-2020 Donald G. Dansereau
+%
+% Enabled nested structs, 2025, Donald Dansereau
 
 function ParentStruct = LFDefaultField( ParentStruct, FieldName, DefaultVal )
 
 %---First make sure the struct exists---
-CheckIfExists = sprintf('exist(''%s'', ''var'') && ~isempty(%s)', ParentStruct, ParentStruct);
-VarExists = evalin( 'caller', CheckIfExists );
-if( ~VarExists )
-    ParentStruct = [];
-else
+try
     ParentStruct = evalin( 'caller', ParentStruct );
+catch ME
+    switch ME.identifier
+        case 'MATLAB:undefinedVarOrClass'
+        case 'MATLAB:UndefinedFunction'
+        case 'MATLAB:nonExistentField'
+        otherwise
+            rethrow(ME); % Unexpected error - rethrow it
+    end
+    ParentStruct = [];
 end
 
 %---Now make sure the field exists---
