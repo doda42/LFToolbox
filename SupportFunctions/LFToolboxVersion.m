decf05bfc420b581e5c981412b08b5496f7784c7
Donald Dansereau <donald.dansereau@gmail.com>
2025-04-24 16:59:43 +1000

    correct version info

--- SupportFunctions/LFToolboxVersion.m
+++ SupportFunctions/LFToolboxVersion.m
@@ -3,4 +3,4 @@
 % Copyright (c) 2013-2020 Donald G. Dansereau
 
 function VersionStr = LFToolboxVersion
-VersionStr = 'development branch for modular calibration';
+VersionStr = 'development branch for v0.6';
